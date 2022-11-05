8b49a0174e64dc792583bdc731bd1cfabd00c27f
Jerko Segvic <jerko.segvic@gmail.com>
2022-11-05 14:45:37 +0100

    l

---
+++ DeepModels.py
@@ -0,0 +1,112 @@
+
+
+
+
+import re
+import spacy
+import pandas as pd
+from nltk.corpus import stopwords
+from collections import Counter
+nlp = spacy.load(
+    "en_core_web_lg"
+)
+df = pd.read_csv("train_all_tasks.csv")
+
+
+
+
+
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+lista_tekst = df["text"]
+lista_label = df["label_sexist"]
+
+for train_index, test_index in sss.split(lista_tekst, lista_label):
+    X_train, X_test = lista_tekst[train_index], lista_tekst[test_index]
+    y_train, y_test = lista_label[train_index], lista_label[test_index]
+    d_train = {"text": X_train, "label": y_train}
+    df_train = pd.DataFrame(d_train)
+    d_test = {"text": X_test, "label": y_test}
+    df_test = pd.DataFrame(d_test)
+
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.125, random_state=42)
+
+lista_tekst = np.array(df_train["text"])
+lista_label = np.array(df_train["label"])
+
+for train_index, valid_index in sss.split(lista_tekst, lista_label):
+    X_train, X_valid = lista_tekst[train_index], lista_tekst[valid_index]
+    y_train, y_valid = lista_label[train_index], lista_label[valid_index]
+    d_train = {"text": X_train, "label": y_train}
+    df_train = pd.DataFrame(d_train)
+    d_valid = {"text": X_valid, "label": y_valid}
+    df_valid = pd.DataFrame(d_valid)
+
+print(len(df_test), len(df_valid), len(df_train))
+
+from gensim.models import KeyedVectors
+from gensim import models
+from gensim.utils import simple_preprocess
+
+word2vec_path = '../GoogleNews-vectors-negative300.bin'
+model = models.KeyedVectors.load_word2vec_format(word2vec_path, binary = True)
+
+
+def W2Vvectorize(train):
+    corpus_text = '\n'.join(train)
+    sentences = corpus_text.split('\n')
+    sentences = [line.lower().split(' ') for line in sentences]
+
+    def clean(s):
+        return [w.strip(',."!?:;()\'') for w in s]
+
+    sentences = [clean(s) for s in sentences if len(s) > 0]
+    vector=[]
+    for sent in sentences:
+        sent_vec=np.zeros(300)
+        count =0
+        for word in sent: 
+            if word in model:
+                vec = model[word]
+                sent_vec += vec 
+                count += 1
+        if count != 0:
+            sent_vec /= count #normalize
+        vector.append(sent_vec)
+    return vector
+
+
+def evaluate(model, text, y_test, target_names=('not sexist', 'sexist')):
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import classification_report
+    from sklearn.metrics import ConfusionMatrixDisplay
+    from sklearn.metrics import precision_recall_fscore_support
+
+    text_features = W2Vvectorize(text)
+    y_test_pred = model.predict(text_features)
+    print(classification_report(y_test, y_test_pred, target_names=target_names))
+
+    precision, recall, f1, _ = precision_recall_fscore_support(y_test, y_test_pred, average="micro")
+    print(f"Precision: {precision:6.2f}")
+    print(f"   Recall: {recall:6.2f}")
+    print(f"       F1: {f1:6.2f}")
+
+    ConfusionMatrixDisplay.from_predictions(y_test, y_test_pred, display_labels=target_names)
+    plt.show()
+
+
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
+lista_label = np.array(df_train["label"])
+lista_label = le.fit_transform(lista_label)
+
+nova_lista = W2Vvectorize(df_train["text"])
+
